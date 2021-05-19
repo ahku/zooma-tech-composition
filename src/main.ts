f7a43693c63a230190f9d71181782e439ade268d
Yeu-Kang Hua <yeukang@gmail.com>
2021-05-19 12:52:51 +0200

    Update

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { method1_ObjectAssign } from './method1-object-assign'
 import { method2_Compose } from './method2-compose-function'
 import { method3_ClassMixins } from './method3-class-mixins'
 
-let output = '<h1>Composition explorations!</h1>\n'
+let output = '<h1>Composition with functions & classes</h1>\n'
 output += method1_ObjectAssign()
 output += method2_Compose()
 output += method3_ClassMixins()
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = `${output}`
+
